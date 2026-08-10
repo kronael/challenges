0d19d09d08bd86e87f9e9e60934ab66e41a9d14c
Ondrej Vostal <ondrej@vostal.net>
2026-08-10 11:42:52 +0000

    fix(41): make range_count O(log n) with rank-augmented skip list

--- 41-hard-ordered-set-queries/golden/main.py
+++ 41-hard-ordered-set-queries/golden/main.py
@@ -7,11 +7,15 @@ P = 0.5
 
 
 class Node:
-    __slots__ = ("val", "next")
+    __slots__ = ("val", "next", "width")
 
     def __init__(self, val, level):
         self.val = val
         self.next = [None] * level
+        # width[i] = number of level-0 nodes from this node (exclusive) up to and
+        # including next[i]; 0 when next[i] is None. Turns range_count into a
+        # difference of ranks, so a full-domain query is O(log n), not O(range).
+        self.width = [0] * level
 
 
 class SkipList:
@@ -19,6 +23,7 @@ class SkipList:
         self.rng = random.Random(seed)
         self.head = Node(None, MAX_LEVEL)
         self.level = 1
+        self.size = 0
 
     def _random_level(self):
         lvl = 1
@@ -36,23 +41,33 @@ class SkipList:
 
     def insert(self, val):
         update = [self.head] * MAX_LEVEL
+        rank = [0] * MAX_LEVEL
         cur = self.head
         for i in range(self.level - 1, -1, -1):
+            rank[i] = 0 if i == self.level - 1 else rank[i + 1]
             while cur.next[i] is not None and cur.next[i].val < val:
+                rank[i] += cur.width[i]
                 cur = cur.next[i]
             update[i] = cur
-        cur = cur.next[0]
-        if cur is not None and cur.val == val:
+        nxt = cur.next[0]
+        if nxt is not None and nxt.val == val:
             return
         lvl = self._random_level()
         if lvl > self.level:
             for i in range(self.level, lvl):
+                rank[i] = 0
                 update[i] = self.head
+                self.head.width[i] = self.size
             self.level = lvl
         node = Node(val, lvl)
         for i in range(lvl):
             node.next[i] = update[i].next[i]
             update[i].next[i] = node
+            node.width[i] = update[i].width[i] - (rank[0] - rank[i])
+            update[i].width[i] = (rank[0] - rank[i]) + 1
+        for i in range(lvl, self.level):
+            update[i].width[i] += 1
+        self.size += 1
 
     def delete(self, val):
         update = [self.head] * MAX_LEVEL
@@ -65,23 +80,26 @@ class SkipList:
         if cur is None or cur.val != val:
             return
         for i in range(self.level):
-            if update[i].next[i] is not cur:
-                break
-            update[i].next[i] = cur.next[i]
+            if update[i].next[i] is cur:
+                update[i].width[i] += cur.width[i] - 1
+                update[i].next[i] = cur.next[i]
+            else:
+                update[i].width[i] -= 1
         while self.level > 1 and self.head.next[self.level - 1] is None:
             self.level -= 1
+        self.size -= 1
+
+    def _count_lt(self, val):
+        cur = self.head
+        rank = 0
+        for i in range(self.level - 1, -1, -1):
+            while cur.next[i] is not None and cur.next[i].val < val:
+                rank += cur.width[i]
+                cur = cur.next[i]
+        return rank
 
     def range_count(self, lo, hi):
-        cur = self.head
-        for i in range(self.level - 1, -1, -1):
-            while cur.next[i] is not None and cur.next[i].val < lo:
-                cur = cur.next[i]
-        cur = cur.next[0]
-        count = 0
-        while cur is not None and cur.val <= hi:
-            count += 1
-            cur = cur.next[0]
-        return count
+        return self._count_lt(hi + 1) - self._count_lt(lo)
 
 
 def solve(ops):
